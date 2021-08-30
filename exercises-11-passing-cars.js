b621ce2cfce8a56360b4f81d8f1c279453617cf1
Mithi Sevilla <mithi.sevilla@gmail.com>
2021-08-30 16:43:25 +0800

    Create exercises-11-passing-cars.js

---
+++ exercises-11-passing-cars.js
@@ -0,0 +1,73 @@
+/*
+
+A non-empty array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
+
+Array A contains only 0s and/or 1s:
+
+0 represents a car traveling east,
+1 represents a car traveling west.
+The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to the west.
+
+For example, consider array A such that:
+
+  A[0] = 0
+  A[1] = 1
+  A[2] = 0
+  A[3] = 1
+  A[4] = 1
+We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
+
+Write a function:
+
+function solution(A);
+
+that, given a non-empty array A of N integers, returns the number of pairs of passing cars.
+
+The function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000.
+
+For example, given:
+
+  A[0] = 0
+  A[1] = 1
+  A[2] = 0
+  A[3] = 1
+  A[4] = 1
+the function should return 5, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer that can have one of the following values: 0, 1.
+
+*/
+
+function solution(A) {
+    const headingEastIndices = A.map((a, index) => {
+        return [a, index]
+    }).filter(x => x[0] === 0).map(y => y[1])
+
+    const headingEastCount = headingEastIndices.length
+    if(headingEastCount === 0 || headingEastCount === A.length) {
+        return 0
+    }
+
+    if(A.length === 2) {
+        return headingEastIndices[0] === 0 ? 1 : 0
+    }
+
+    let passingCarsCount = 0
+
+
+    for (let eastArrayIndex in headingEastIndices) {
+        const eastIndex = headingEastIndices[eastArrayIndex]
+        const carsHeadingEastAhead =  headingEastCount - eastArrayIndex - 1
+        const carsAhead = A.length - 1 - eastIndex
+        const carsHeadingWestAhead = carsAhead - carsHeadingEastAhead
+        passingCarsCount = passingCarsCount + carsHeadingWestAhead
+        if(passingCarsCount > 1000000000) {
+            return -1
+        }
+    }
+
+    return passingCarsCount
+}
